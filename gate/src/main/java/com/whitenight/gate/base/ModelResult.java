f64297966443dbb35e7cbad9e0b9313d9aa23264
czy <zhiyang.chen@ezhiyang.com>
2019-11-28 14:21:40 +0800

    #添加统一响应对象

---
+++ gate/src/main/java/com/whitenight/gate/base/ModelResult.java
@@ -0,0 +1,45 @@
+package com.whitenight.gate.base;
+
+import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 接口响应工具类
+ */
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ModelResult implements Serializable {
+    private String msg;
+    private String code;
+    private Object data;
+
+    public static ModelResult getSuccess(String msg){
+        return new ModelResult(msg
+            ,ResponseCodeEnum.SUCCESS_CODE.getCode()
+            ,null);
+    }
+
+    public static ModelResult getSuccess(String msg, Object data){
+        return new ModelResult(ResponseCodeEnum.SUCCESS_CODE.getCodeMsg()
+            ,ResponseCodeEnum.SUCCESS_CODE.getCode()
+            ,data);
+    }
+
+    public static ModelResult getError(String msg){
+        return new ModelResult(ResponseCodeEnum.SUCCESS_CODE.getCodeMsg()
+            ,ResponseCodeEnum.SUCCESS_CODE.getCode()
+            ,null);
+    }
+
+    public static ModelResult getError(String msg, Object data){
+        return new ModelResult(ResponseCodeEnum.ERROR_CODE.getCodeMsg()
+            ,ResponseCodeEnum.ERROR_CODE.getCode()
+            ,data);
+    }
+}
---
+++ gate/src/main/java/com/whitenight/gate/base/ResponseCodeEnum.java
@@ -0,0 +1,37 @@
+package com.whitenight.gate.base;
+
+import lombok.AllArgsConstructor;
+
+
+@AllArgsConstructor
+public enum ResponseCodeEnum {
+    SUCCESS_CODE("成功","200"),
+    ERROR_CODE("失败","400");
+
+    private String codeMsg;
+    private String code;
+
+    public String getCodeMsg() {
+        return codeMsg;
+    }
+
+    public void setCodeMsg(String codeMsg) {
+        this.codeMsg = codeMsg;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    @Override
+    public String toString() {
+        return "ResponseCodeEnum{" +
+            "codeMsg='" + codeMsg + '\'' +
+            ", code='" + code + '\'' +
+            '}';
+    }
+}
