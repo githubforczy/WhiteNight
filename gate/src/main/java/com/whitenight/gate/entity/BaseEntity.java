362f7967cfc829043348e830577b3c3f248d047f
czy <zhiyang.chen@ezhiyang.com>
2019-11-28 14:23:15 +0800

    #提取出公共基础字段对象

---
+++ gate/src/main/java/com/whitenight/gate/entity/BaseEntity.java
@@ -0,0 +1,38 @@
+package com.whitenight.gate.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Data;
+
+@Data
+public class BaseEntity implements Serializable {
+    /**
+     * 主键id
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    /**
+     * 创建人
+     */
+    private Long createdBy;
+    /**
+     * 创建时间
+     */
+    private Date createdTime;
+    /**
+     * 更新人
+     */
+    private Long updatedBy;
+    /**
+     * 更新时间
+     */
+    private Date updatedTime;
+    /**
+     * 是否删除
+     */
+    private Integer isDeleted;
+}
